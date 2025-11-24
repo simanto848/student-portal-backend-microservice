4b55ff26cef91b1650294ce2401da6e86c85d049
Simanto Hasan <75442244+simanto848@users.noreply.github.com>
2025-11-24 20:26:39 +0600

    refactor: Remove facilities service and add gateway proxy routes for notification, communication, and classroom.

--- gateway/app.js
+++ gateway/app.js
@@ -15,6 +15,9 @@ app.use('/api/academic', expressProxy(process.env.ACADEMIC_SERVICE_URL));
 app.use('/api/user', expressProxy(process.env.USER_SERVICE_URL));
 app.use('/api/library', expressProxy(process.env.LIBRARY_SERVICE_URL));
 app.use('/api/enrollment', expressProxy(process.env.ENROLLMENT_SERVICE_URL));
+app.use('/api/notification', expressProxy(process.env.NOTIFICATION_SERVICE_URL));
+app.use('/api/communication', expressProxy(process.env.COMMUNICATION_SERVICE_URL));
+app.use('/api/classroom', expressProxy(process.env.CLASSROOM_SERVICE_URL));
 
 app.listen(PORT, () => {
     console.log(`Gateway server started on http://localhost:${PORT}`.green.underline.bold);
