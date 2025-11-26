4ff8e6ba50255d9a8d1a7eba91e04477217bea83
Simanto Hasan <75442244+simanto848@users.noreply.github.com>
2025-11-26 16:49:14 +0600

    feat: Add cookie-parser middleware for handling cookies.

--- academic/app.js
+++ academic/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import apiRoutes from "./routes/index.js";
 import { ApiResponse, ApiError } from 'shared';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/", apiRoutes);
 
